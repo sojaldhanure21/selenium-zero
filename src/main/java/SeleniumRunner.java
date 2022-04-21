c57753db0838657a5f81ea02fc9108b3bdc28b11
Sojal-Lappy <sdhanure8@gmail.com>
2022-04-22 01:59:15 +0530

    initial commit

---
+++ src/main/java/SeleniumRunner.java
@@ -0,0 +1,13 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumRunner {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver webDriver= new ChromeDriver() ;
+        webDriver.get("https://www.google.com/");
+//Establishing the webdriver
+
+    }
+}
